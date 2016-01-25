aa1247ac0e5d2912a184def883a13853ff1ff392
Dennis Lysenko <dennislysenko@hotmail.com>
2016-01-25 15:56:58 -0500

    Setup Alpha build counter

--- src/org/planetservers/ps/core/JavaPanel.java
+++ src/org/planetservers/ps/core/JavaPanel.java
@@ -1,10 +1,17 @@
 package org.planetservers.ps.core;
 
+import java.awt.Dimension;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class JavaPanel extends JPanel {
 
-	
+	public JavaPanel() {
+		super();
+		setFocusable(true);
+		requestFocus();
+		
+		setPreferredSize(new Dimension(500, 500));
+	}
 	
 }
--- src/org/planetservers/ps/core/Main.java
+++ src/org/planetservers/ps/core/Main.java
@@ -1,17 +1,50 @@
 package org.planetservers.ps.core;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		JFrame f = new JFrame("PlanetServers a001");
+		JFrame f = new JFrame();
+		f.setTitle("PlanetServers a" + (getBuild() + 1));
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.add(new JavaPanel());
 		f.setVisible(true);
 		f.pack();
 		f.setResizable(false);
 		f.setLocationRelativeTo(null);
+		updateBuild();
+	}
+	
+	private static Integer getBuild() {
+		Integer output = null;
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(new File("alpha.build")));
+			output = Integer.parseInt(reader.readLine());
+			reader.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return output;
+	}
+	
+	private static void updateBuild() {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(new File("alpha.build")));
+			int build = Integer.parseInt(reader.readLine());
+			reader.close();
+			PrintWriter writer = new PrintWriter(new File("alpha.build"));
+			writer.println(build + 1);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
